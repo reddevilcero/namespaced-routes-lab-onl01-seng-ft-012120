c6df53fc611c7365f71d2d82a9a88b66d52f671c
reddevilcero <reddevil_cero@hotmail.com>
2020-03-23 17:35:50 +0000

    added form to change preference

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,6 +1,23 @@
 class Admin::PreferencesController < ApplicationController
   
   def index 
+    @preference = Preference.first
+  end
+
+  def update
+    @preference = Preference.first
+      if @preference.update_attributes(preference_params)
+        flash[:success] = "Preference was successfully updated"
+        redirect_to admin_preferences_path
+      else
+        flash[:error] = "Something went wrong"
+        redirect_to admin_preferences_path
+      end
   end
   
+  private 
+
+  def preference_params
+    params.require(:preference).permit(:allow_create_artists,:allow_create_songs)
+  end
 end
